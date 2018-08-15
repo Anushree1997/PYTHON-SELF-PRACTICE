8683bc647a42a19b1af9b3bee6e2d44766d2c709
Anushree1997 <40564820+Anushree1997@users.noreply.github.com>
2018-08-16 00:09:51 +0530

    Create arithmetic_operations.py

---
+++ day1/arithmetic_operations.py
@@ -0,0 +1,48 @@
+
+Problem Statement
+
+In the coding window displayed below, write suitable arithmetic expressions for the displayed descriptions.
+
+Ideal Solution
+
+1. Sum of 21 and 62
+
+21+62
+
+2. Difference between 43 and 17
+
+43-17
+
+3. 36 mod 5
+
+35%5
+
+4. Sum of 6 and 4 multiplied by 10
+
+(6+4)*10
+
+5. 23 to the 5th power
+
+23**5
+
+6. An expression that will result in 19 and which includes multiplication
+
+4*5-1
+
+7. Sum of 177 mod 4 and 223 mod 5
+
+(177%4)+(223%5)
+
+8. Difference between 3rd power of 14 and 3rd power of 13
+
+14**3-13**3
+
+9. An expression that will result in 0, which does not include multiplication and contains at least 3 arithmetic operators
+
+(27%11)+10-15
+
+10. An expression that includes all six arithmetic operators, addition (+), subtraction (-), multiplication (*), division (/), modulo (%) and power (**)
+
+((20*3+5-8)/3**2)%4
+
+
