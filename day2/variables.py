2f84add3efd1c3064e955d21fdb06dfa8a9a59d5
Anushree1997 <40564820+Anushree1997@users.noreply.github.com>
2018-08-16 00:13:51 +0530

    Create variables.py

---
+++ day2/variables.py
@@ -0,0 +1,19 @@
+a = input("Enter any number: ")
+
+b = input("Enter any number: ")
+
+print(a+b)
+
+>>> a = input("Enter any number: ")
+
+Enter any number: 7
+
+>>> b = input("Enter any number: ")
+
+Enter any number: 1
+
+>>> print(a+b)
+
+71
+
+Explanation: By default, the input() function in Python takes values as a string. So when we use the addition operator, +, on these values, they are concatenated or joined rather than added. 
