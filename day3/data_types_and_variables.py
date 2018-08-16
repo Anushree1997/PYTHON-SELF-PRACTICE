a892ac6794e0b27719dc3eb5bc58e139732c66b3
Anushree1997 <40564820+Anushree1997@users.noreply.github.com>
2018-08-16 22:17:42 +0530

    Create data_types_and_variables.py

---
+++ day3/data_types_and_variables.py
@@ -0,0 +1,37 @@
+Verify the data type for each of the objects given below using the type() function.
+
+55.50
+
+6+4j
+
+"hello"
+
+ ([1,2,3,4])
+
+ ((1,2,3,4))
+
+ ({1:"one", 2:"two", 3:"three"})
+
+>>> type(55.50)
+
+<class 'float'>
+
+>>> type(6+4j)
+
+<class 'complex'>
+
+>>> type("hello")
+
+<class 'str'>
+
+>>> type([1,2,3,4])
+
+<class 'list'>
+
+>>> type((1,2,3,4))
+
+<class 'tuple'>
+
+>>> type({1:"one", 2:"two", 3:"three"})
+
+<class 'dict'>
